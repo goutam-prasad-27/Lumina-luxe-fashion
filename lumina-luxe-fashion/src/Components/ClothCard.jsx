fc4496589bb83725ba9924f5f991ccd4426bd40b
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-08-31 19:15:30 +0530

    cloth card text adjust

--- lumina-luxe-fashion/src/Components/ClothCard.jsx
+++ lumina-luxe-fashion/src/Components/ClothCard.jsx
@@ -4,10 +4,10 @@ function ClothCard({ project }) {
 			<img
 				src={project.image}
 				alt={project.title}
-				className="rounded-3x p-2"
+				className="rounded-3xl p-2"
 			/>
 			<div className="p-4">
-				<h3 className="mb-2 text-2xl font-bold tracking-tighter">
+				<h3 className="mb-2 text-2xl font-bold tracking-[.1rem]">
 					{project.title}
 				</h3>
 				<p className="text-sm"> {project.description}</p>
