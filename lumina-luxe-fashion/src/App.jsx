92b6da2f9999b7e5fc946821af5b9b7fe4d86a3f
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-09-01 22:32:27 +0530

    Shop Section added

--- lumina-luxe-fashion/src/App.jsx
+++ lumina-luxe-fashion/src/App.jsx
@@ -3,15 +3,18 @@ import HeroSection from "./Components/HeroSection";
 import Navbar from "./Components/Navbar";
 import ClothingSection from "./Components/ClothingSection";
 import AboutSection from "./Components/AboutSection";
-
+import MissionSection from "./Components/MissionSection";
+import ShoppingSection from "./Components/ShoppingSection";
 
 function App() {
 	return (
 		<main className="overflow-y-hidden text-neutral-200 antialiased">
-			<HeroSection/>
-			<Navbar/>
-			<ClothingSection/>
-			<AboutSection/>
+			<HeroSection />
+			<Navbar />
+			<ClothingSection />
+			<AboutSection />
+			<MissionSection />
+			<ShoppingSection />
 		</main>
 	);
 }
---
+++ lumina-luxe-fashion/src/Components/ShoppingSection.jsx
@@ -0,0 +1,37 @@
+import { Shopping } from "../Assets/AssetsLink";
+
+function ShoppingSection() {
+	return (
+		<section id="shopping">
+			<h2 className="my-8 text-center text-3x lg:text-4xl tracking-[.2rem]">
+				Shop Here
+			</h2>
+			<div className="container mx-auto px-4">
+				{Shopping.map((shop, idx) => (
+					<div
+						className="flex items-center border-b-4 border-dotted border-neutral-700/40 py-2"
+						key={idx}
+					>
+						<div className="w-1/3 flex-shrink-0">
+							<img
+								src={shop.image}
+								alt="shopping"
+								className="h-[22rem] rounded-3xl aspect-square object-cover"
+							/>
+						</div>
+						<div className=" pl-8">
+							<h3 className="text-2xl uppercase tracking-[.1rem] text-rose-300">
+								{shop.title}
+							</h3>
+							<p className="mt-4 text-lg tracking-tighter">
+								{shop.description}
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</section>
+	);
+}
+
+export default ShoppingSection;
