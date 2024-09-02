31b2eb4ca694e0b22cae6cc1ab02cc8e0905a74f
Goutam Prasad <goutam.prasad.2707@gmail.com>
2024-09-02 13:42:42 +0530

    copyright details added

--- lumina-luxe-fashion/src/Components/FooterSection.jsx
+++ lumina-luxe-fashion/src/Components/FooterSection.jsx
@@ -15,6 +15,9 @@ function FooterSection() {
 					</a>
 				))}
 			</div>
+			<p className="mt-8 text-center text-neutral-500">
+				&copy;Lumina Luxe ~ Copyright 2024 ~ All rights reserved.
+			</p>
 		</div>
 	);
 }
